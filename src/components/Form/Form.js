27d45ccbd4b3207f2a551f9fba66ff83f2c5b00a
TamannaSarker <67562599+TamannaSarker@users.noreply.github.com>
2021-08-31 15:46:23 +0200

    form-is-done

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,8 +1,41 @@
 import React from 'react';
+import { Link} from 'react-router-dom';
+import './Form.css'
+
+
 
 function Form () {
+
+      
     return (
-       <div>this is form</div>
+       <div className="form-container">
+           
+          
+           <h1>
+               Registration
+           </h1>
+           <form action="">
+               <div>
+               <label htmlFor="name">Name:</label>
+               <input type="text" name="name" id="name" placeholder="type your name" required />
+               </div>
+               <div>
+               <label htmlFor="email">Email:</label>
+               <input type="text" name="email" id="email" placeholder="type your email" required />
+               </div>
+               <div>
+               <label htmlFor="name">Time:</label>
+               <input type="number" name="time" id="time" required />
+               </div>
+               <div>
+                <Link to="/confirmation">
+                <button>Confirm</button>
+                </Link>
+               
+               </div>
+
+           </form>
+       </div>
     );
 };
 
